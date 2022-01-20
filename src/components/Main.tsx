4f7418405c31ec0e5d95f9a5f139bdc7c0823175
djtoler <84260551+djtoler@users.noreply.github.com>
2022-01-20 17:01:15 -0500

    main pg work

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,32 @@
+import {useEffect, useState} from 'react';
+import {MovieResponse, Results} from "../model/MovieModel";
+import {fetchTMDB} from "../Service/tmdbService";
+import ResultList from './ResultList';
+import SearchForm from './SearchForm';
 
-function Main(){
+const Main = () => {
+// set variable & state function for movie display
+const [movies, setMovies] = useState<Results[]>([]);
+// set variables & state function for form searches
+const [formSearch, setFormSearch] = useState("");
+
+useEffect(() => {
+    fetchTMDB()
+    .then((data) => setMovies(data))
+}, [])
+
+
+return (
+    <div>
+        <h1>Movie API</h1>
+        <ResultList />
+        <SearchForm />
+
+
+    </div>
+)
+
+
 
 }
 export default Main;
