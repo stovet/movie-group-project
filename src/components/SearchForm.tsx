3911872e9b1d2a5bef5d69579219c6b749e59123
stovet <steven.topalian@gmail.com>
2022-01-20 16:42:21 -0500

    some css for SearchForm

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -18,12 +18,18 @@ function SearchForm(onSubmit: Props){
     return (
         <div className="SearchForm">
             <form onSubmit={handleFormSubmit}>
-                <label htmlFor="rating">Rating: </label>
-                <input type="number" name="rating" id="rating" min={0} max={10} step={.1} placeholder="0.0" onChange={e => setRating(parseFloat(e.target.value))}/>
-                <label htmlFor="runTime">Run time:</label>
-                <input type="number" name="runTime" id="runTime" onChange={e => setRunTime(parseInt(e.target.value))}/>
-                <label htmlFor="genre">Genre:</label>
-                <input type="text" name="genre" id="genre" onChange={e => setGenre(e.target.value)}/>
+                <div className="inputContainer">
+                    <label htmlFor="rating">Rating: </label>
+                    <input type="number" name="rating" id="rating" min={0} max={10} step={.1} placeholder="0.0" onChange={e => setRating(parseFloat(e.target.value))}/>
+                </div>
+                <div className="inputContainer">
+                    <label htmlFor="runTime">Run time:</label>
+                    <input type="number" name="runTime" id="runTime" onChange={e => setRunTime(parseInt(e.target.value))}/>
+                </div>
+                <div className="inputContainer">
+                    <label htmlFor="genre">Genre:</label>
+                    <input type="text" name="genre" id="genre" onChange={e => setGenre(e.target.value)}/>
+                </div>
                 <button type="submit">Search</button>
             </form>
         </div>
