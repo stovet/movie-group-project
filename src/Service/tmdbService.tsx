ca4853eea85556a54405f0186973a8311c3e9b91
stovet <steven.topalian@gmail.com>
2022-02-25 08:30:01 -0500

    hid api key

--- src/Service/tmdbService.tsx
+++ src/Service/tmdbService.tsx
@@ -5,12 +5,12 @@ export function fetchGenreTMDB(
   page: number,
   genre: number
 ): Promise<Results[]> {
-  const api_key = process.env.REACT_API_TMDB_KEY || "";
+  const api_key = process.env.REACT_APP_TMDB_KEY || "";
   console.log(genre);
   return axios
     .get("https://api.themoviedb.org/3/discover/movie", {
       params: {
-        api_key: "b80ce08c456bca9bb040f009efb1aaf9",
+        api_key: api_key,
         page: page,
         with_genres: genre,
       },
